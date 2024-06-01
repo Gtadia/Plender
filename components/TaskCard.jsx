1c6e78d3a470fc57507fe62d49f30db7ee0b4e74
Min Han <m235humit9@gmail.com>
2024-05-31 20:32:06 -0400

    created basic task card layout

--- components/TaskCard.jsx
+++ components/TaskCard.jsx
@@ -2,29 +2,43 @@ import { StyleSheet, Text, View, Dimensions } from 'react-native'
 import React, { useState } from 'react'
 
 const TaskCard = ({ task }) => {
-  const [tags, setTags] = useState(['hi', 'bye']);
+  const [tags, setTags] = useState([]);
 
   return (
     // todo — should I add a border style or not?
     <View style={{...styles.container, ...styles.borderStyle}}>
-      <View>
-        <Text>
-          {"test text"}
-        </Text>
+        <View style={{position: 'absolute', width: '50%', height: '100%', backgroundColor: 'green', left: 0, top: 0, ...styles.borderStyle}}></View>
 
-        <View style={{backgroundColor: 'transparent', width: 150, height: 40, flexDirection: 'row'}}>
-          {
-            tags.map((tag) => (
+        <View style={{justifyContent: 'center',}}>
+          <Text>
+            {"test text"}
+          </Text>
+
+          <View style={{backgroundColor: 'transparent', width: 150, height: 40, flexDirection: 'row', alignItems: 'center'}}>
+            {
+              tags.map((tag) => (
+                <View>
+                  <Text>{tag}</Text>
+                </View>
+              ))
+            }
+            {
+              tags.length === 0  &&
               <View>
-                <Text>{tag}</Text>
+                <Text>add tag</Text>
               </View>
-            ))
-          }
-          <View>
-            <Text>Hello</Text>
+            }
           </View>
         </View>
-      </View>
+
+        <View style={{}}>
+          <View>
+            <Text>{"1:38"}</Text>
+          </View>
+          <View>
+            <Text style={{fontStyle: 'italic'}}>{"/2:00"}</Text>
+          </View>
+        </View>
     </View>
   )
 }
@@ -36,12 +50,18 @@ const styles = StyleSheet.create({
     width: Dimensions.get('window').width * 0.9,
     height: 70,
 
-    justifyContent: "center",
+    justifyContent: "space-around",
+    flexDirection: 'row',
+    alignItems: 'center',
 
     backgroundColor: "gray",
 
   },
   borderStyle: {
     borderRadius: 12
+  },
+  test: {
+    borderWidth: 2,
+    borderColor: 'red'
   }
 })
